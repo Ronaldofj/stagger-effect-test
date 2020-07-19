63e785f8f7f9b335b9619905f76a8e89b903df5b
Ronaldo <ronaldofigueiredosp@gmail.com>
2020-07-19 15:08:48 -0300

    Setting styled-components and global style

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-border-box;
+  }
+
+  body {
+    background-color: #f1f1f1;
+  }
+`;
+
+export default GlobalStyle;
