a4bb3118e4a55aeb95cf9e496d84301ea33b3f46
Ronaldo <ronaldofigueiredosp@gmail.com>
2020-07-22 09:26:22 -0300

    Making the stagger effect in course list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Container from './components/Container';
+import Button from './components/Button';
+import ListItem from './components/ListItem';
 
 const App: React.FC = () => {
+  const [cursos, setCursos] = useState(['React', 'React Native', 'NodeJS']);
+  const [visibility, setVisibility] = useState(false);
+
+  function showList(): void {
+    setVisibility(!visibility);
+  }
+
+  function makeDelay(index: number): number {
+    let delay = 0;
+
+    if (visibility) {
+      delay = index * 100;
+    } else {
+      delay = (cursos.length - index - 1) * 100;
+    }
+
+    return delay;
+  }
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <div />
-    </div>
+    <Container>
+      <h1>Cursos - Stagger Effect</h1>
+      <Button type="button" onClick={() => showList()}>
+        {visibility ? 'Ocultar lista' : 'Mostrar lista'}
+      </Button>
+      <ul>
+        {cursos.map((curso, index) => (
+          <ListItem active={visibility} delay={makeDelay(index)}>
+            {curso}
+          </ListItem>
+        ))}
+      </ul>
+    </Container>
   );
 };
 
